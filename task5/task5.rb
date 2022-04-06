1138bcc555c4a20b038b893a000a46d7b3e52935
SunSof <buketovasofi@gmail.com>
2022-04-06 23:19:53 +0400

    task5.1

--- task5/task5.rb
+++ task5/task5.rb
@@ -1,19 +1,51 @@
 require 'csv'
 
-#5.1
-class Movie 
-    def movie_hash(file_name: 'movies.txt')
-      hash_keys = %w[links title year country date genre time rating directors actors]
-      CSV.parse(File.read(file_name), col_sep: '|', headers: hash_keys).map(&:to_h)
+# 5.1
+# class MovieCollection
+#   def all(file_name)
+#     hash_keys = %w[links title year country date genre time rating directors actors]
+#     CSV.parse(File.read(file_name), col_sep: '|', headers: hash_keys).map(&:to_h)
+#   end
+# end
+
+# movie_collection = MovieCollection.new
+# p movie_collection.all("movies.txt")
+
+class Movie
+  def initialize(hash)
+    @links = hash[:links]
+    @title = hash[:title]
+    @year = hash[:year]
+    @country = hash[:country]
+    @date = hash[:date]
+    @genre = hash[:genre]
+    @time = hash[:time]
+    @rating = hash[:rating]
+    @directors = hash[:directors]
+    @actors = hash[:actors]
+  end
+end
+
+class MovieCollection
+  attr_accessor :file_name
+
+  def movies_parse(file_name)
+    hash_keys = %w[links title year country date genre time rating directors actors]
+    CSV.parse(File.read(file_name), col_sep: '|', headers: hash_keys).map do |el|
+      hash = el.to_h.transform_keys(&:to_sym)
+      Movie.new(hash)
     end
   end
- 
 
-  class MovieCollection < Movie
-    
+  def initialize(file_name)
+    @file_name = file_name
+    @collection = movies_parse(file_name)
   end
 
+  def all
+    @collection
+  end
+end
 
-  movies = Movie.new()
-  movie_collection = MovieCollection.new()
-  movie_collection.movie_hash
+movie_collection = MovieCollection.new('movies.txt')
+p movie_collection.all
---
+++ task5/test.rb
@@ -0,0 +1,53 @@
+# class Cat
+# def initialize(legs_count)
+#   @legs = legs_count
+# end
+
+# attr_readr :legs
+# def legs
+#   @legs
+# end
+
+# attr_writer :legs
+# def legs=(new_legs)
+#   @legs = new_legs
+# end
+
+#   attr_accessor :legs
+# end
+
+# cat = Cat.new
+# cat.legs = 5
+# p cat
+
+class BirdsCollection
+  attr_accessor :colection
+
+  def initialize(collection)
+    @collection = collection
+  end
+
+  def col
+    c = @collection
+    c.map { |el| el.transform_keys(&:to_sym) }
+  end
+end
+
+class Bird
+  attr_reader :name
+
+  def initialize(hash)
+    @name = hash['name']
+    @color = hash['color']
+  end
+end
+
+birds_hash = [{ 'name' => 'sinica', 'color' => 'yellow' }, { 'name' => 'dytel', 'color' => 'red' }]
+a = birds_hash.map do |hash|
+  Bird.new(hash)
+end
+p a
+
+# bird = Bird.new(birds_hash[0])
+# bird.name
+# bird2 = Bird.new(birds_hash[1])
